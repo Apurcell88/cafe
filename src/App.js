c47bf230a4fe2fff086a4501de2b91e5baeba3a6
Adam Purcell <apurcell88@gmail.com>
2024-03-01 15:17:55 -0600

    Added Landing component and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Nav from "./Components/Nav";
+import Landing from "./Components/Landing";
 
 function App() {
   return (
     <div className="App">
       <div className="overlay"></div>
       <Nav />
+      <Landing />
     </div>
   );
 }
